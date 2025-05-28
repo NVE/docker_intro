74b69a43d6eff39554ac898e3cf524795ee036d4
Andreas Bade <anba@nve.no>
2025-05-28 07:43:32 +0000

    Fikset linjeskift i git

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,3 +1,5 @@
 echo "Fikser linjeskift og rettigheter...."
 sudo dos2unix /workspaces/docker_intro/skripter/konverterfiler.sh
 sudo /workspaces/docker_intro/skripter/konverterfiler.sh
+git config --global core.autocrlf false
+git reset --hard
